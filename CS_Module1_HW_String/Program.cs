0d3852dce7cd41aad50971e70d0e647d179436f9
karpov2713 <karpov2713@gmail.com>
2024-02-02 22:45:01 +0300

    Оформил решение ДЗ к модулю 1: работа со строками.

--- CS_Module1_HW_String/Program.cs
+++ CS_Module1_HW_String/Program.cs
@@ -10,6 +10,22 @@ namespace CS_Module1_HW_String
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Сколько Вам лет? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Какой Ваш знак зодиака? ");
+            string zodiacSign = Console.ReadLine();
+
+            Console.Write("Кто вы по профессии? ");
+            string profession = Console.ReadLine();
+
+            Console.Write("В каком городе вы живете? ");
+            string placeOfResidence = Console.ReadLine();
+
+            Console.WriteLine("Ваше имя {0}, Вам {1} лет, {2} по гороскопу, живете в городе {3}, по профессии Вы - {4}.", name, age, zodiacSign, placeOfResidence, profession);
 
         }
     }
