c53bd9e75e1c8063814fda093361ff850c300a44
karpov2713 <karpov2713@gmail.com>
2024-02-02 23:48:50 +0300

    ДЗ Картинки. Выполнено.

---
+++ CS_Module1_HW_Pictures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CS_Module1_HW_Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfImages = 52;
+            int imagesInRow = 3;
+            int amountOfFilledRows = amountOfImages / imagesInRow;
+            int remainingImages = amountOfImages % imagesInRow;
+
+            Console.WriteLine($"Полностью заполнено {amountOfFilledRows} рядов, сверх меры {remainingImages} картинка.");
+
+        }
+    }
+}
