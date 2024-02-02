e78c77aff0019f6fd3699f5197b9b395512cfa41
karpov2713 <karpov2713@gmail.com>
2024-02-02 23:10:14 +0300

    Новый проект с условными операторами и циклами. Первый пример.

---
+++ OperatorsAndLoops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAndLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.Write("Введите свой возраст? ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Добро пожаловать в наш бар.");
+                Console.WriteLine("Что будете пить?");
+            }
+            else
+            {
+                Console.WriteLine("Вы слишком юны.");
+                Console.WriteLine("Приходите к нам через: " + (18 - age) + " лет.");
+            }
+        }
+    }
+}
