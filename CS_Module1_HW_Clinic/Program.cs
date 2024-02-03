2d76f3be5126989be34ec3f34630b0ddde9b978d
karpov2713 <karpov2713@gmail.com>
2024-02-03 19:32:23 +0300

    Решение задачи про очередь в поликлинике.

---
+++ CS_Module1_HW_Clinic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Module1_HW_Clinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesForOneClient = 10;
+            int amountPeopleInClinic;
+            int hoursToStay;
+            int minutesToStay;
+            int minutesInOneHour = 60;
+            int totalTimeInMinutes;
+
+            Console.Write("Сколько людей в очереди вы видите? ");
+            amountPeopleInClinic = Convert.ToInt32(Console.ReadLine());
+
+            totalTimeInMinutes = amountPeopleInClinic * minutesForOneClient;
+            hoursToStay = totalTimeInMinutes / minutesInOneHour;
+            minutesToStay = totalTimeInMinutes % minutesInOneHour;
+
+            Console.WriteLine($"Вы должны простоять в очереди {hoursToStay} часа и {minutesToStay} минут.");
+        }
+    }
+}
---
+++ CS_Module1_HW_Clinic/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CS_Module1_HW_Clinic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CS_Module1_HW_Clinic")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9cff1d68-87ce-497a-83e0-2c311173fc42")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
