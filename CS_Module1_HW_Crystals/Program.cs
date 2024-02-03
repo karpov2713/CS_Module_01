1c663bd70125aa337b21007e7033da8a5185cb18
karpov2713 <karpov2713@gmail.com>
2024-02-03 17:50:21 +0300

    Решение задачи с кристаллами.

---
+++ CS_Module1_HW_Crystals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CS_Module1_HW_Crystals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int goldInWallet;
+            int priceForOneCrystal = 5;
+            int amountOfCrystals;
+            int costOfCrystals;
+
+            Console.Write("Сколько у вас золота? ");
+            goldInWallet = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Цена одного кристалл {priceForOneCrystal}.");
+            Console.Write("Сколько кристаллов вы хотите купить? ");
+            amountOfCrystals = Convert.ToInt32(Console.ReadLine());
+
+            costOfCrystals = priceForOneCrystal * amountOfCrystals;
+            goldInWallet -= costOfCrystals;
+            Console.WriteLine($"Вы приобрели {amountOfCrystals} кристаллов, у вас осталось {goldInWallet} золотых.");
+        }
+    }
+}
