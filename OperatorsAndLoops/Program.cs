886095f729257c749bf9d98eefbd869ef7872339
karpov2713 <karpov2713@gmail.com>
2024-02-04 22:18:25 +0300

    ДЗ Сумма чисел подготовлено.

--- OperatorsAndLoops/Program.cs
+++ OperatorsAndLoops/Program.cs
@@ -265,6 +265,33 @@ namespace OperatorsAndLoops
                 Console.Write(i + " ");
             }*/
 
+            // Сумма чисел
+            /*Random random = new Random();
+
+            int number;
+            number = random.Next(0, 100);
+            int sumOfDivisible_3 = 0;
+            int sumOfDivisible_5 = 0;
+            int sumOfDiv3AndDiv5 = 0;
+
+            for (int i = 0; i <= number; i++) 
+            {
+                if (i % 3 == 0)
+                {
+                    sumOfDivisible_3 += i;
+                }
+                else if (i % 5 == 0)
+                {
+                    sumOfDivisible_5 += i;
+                }
+                sumOfDiv3AndDiv5 = sumOfDivisible_3 + sumOfDivisible_5;
+            }
+
+            Console.WriteLine($"Случайное число = {number}.");
+            Console.WriteLine($"Сумма чисел кратных 3 = {sumOfDivisible_3}.");
+            Console.WriteLine($"Сумма чисел кратных 5 = {sumOfDivisible_5}.");
+            Console.WriteLine($"Общая сумма кратных чисел = {sumOfDiv3AndDiv5}.");*/
+
 
         }
     }
