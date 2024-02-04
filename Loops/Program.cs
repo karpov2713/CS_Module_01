bff9eafe33ac8494f9d36d7bba5ab12a9863d553
karpov2713 <karpov2713@gmail.com>
2024-02-04 21:09:40 +0300

    Новый проект с ДЗ Освоение циклов

---
+++ Loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int messageCount;
+
+            Console.Write("Добрый день. Какое сообщение вывести? ");
+            message = Console.ReadLine();
+
+            Console.Write("Сколько раз вывести сообщение? ");
+            messageCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < messageCount; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
