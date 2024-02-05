63cb2ee9ee08268237649abecde88c4ec85f702b
karpov2713 <karpov2713@gmail.com>
2024-02-05 15:02:05 +0300

    Создал новый проект для ДЗ. Контроль выхода

---
+++ CS_Module1_HW_ControlOfExit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CS_Module1_HW_ControlOfExit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+
+            while (true)
+            {
+                Console.WriteLine("Введите ваше сообщение.");
+                Console.WriteLine("Для выхода из программы введите: \"exit\" или \"выход\".");
+                message = Console.ReadLine();
+
+                if (message == "exit" || message == "выход")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ CS_Module1_HW_ControlOfExit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CS_Module1_HW_ControlOfExit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CS_Module1_HW_ControlOfExit")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0a325c44-651a-4db9-b2f1-43001ddafd3f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
