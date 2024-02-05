fd324b44b9ce044d77c603187565e2c615b14bc8
karpov2713 <karpov2713@gmail.com>
2024-02-05 17:27:25 +0300

    ДЗ Последовательность. Первая попытка сдать.

---
+++ CS_Module1_HW_Sequence/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CS_Module1_HW_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 5;
+            int step = 7;
+            int lastNumber = 96;
+
+            // Цикл FOR(){} выбран, т.к. является параметрическим.
+            // В нашей задаче, четко указаны параметры и этот цикл наиболее подходящий.
+            for (int i = firstNumber; i <= lastNumber; i += step)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
