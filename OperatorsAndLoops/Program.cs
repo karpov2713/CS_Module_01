bbe193a9929481b47af24c0e7e757f4d60deef10
karpov2713 <karpov2713@gmail.com>
2024-02-06 23:49:24 +0300

    ДЗ Сумма чисел. Сохранил в основном файле.

--- OperatorsAndLoops/Program.cs
+++ OperatorsAndLoops/Program.cs
@@ -269,30 +269,32 @@ namespace OperatorsAndLoops
             /*Random random = new Random();
 
             int number;
-            int maxValue = 100;
-            number = random.Next(0, maxValue);
+            int maxValue = 101;
             int sumOfDivisible_3 = 0;
             int sumOfDivisible_5 = 0;
             int sumOfDiv3AndDiv5 = 0;
+            int figure_3 = 3;
+            int figure_5 = 5;
+
+            number = random.Next(0, maxValue);
 
             for (int i = 0; i <= number; i++)
             {
-                if (i % 3 == 0)
+                if (i % figure_3 == 0)
                 {
                     sumOfDivisible_3 += i;
                 }
-                else if (i % 5 == 0)
+                else if (i % figure_5 == 0)
                 {
                     sumOfDivisible_5 += i;
                 }
+
                 sumOfDiv3AndDiv5 = sumOfDivisible_3 + sumOfDivisible_5;
             }
 
             Console.WriteLine($"Случайное число = {number}.");
-            Console.WriteLine($"Сумма чисел кратных 3 = {sumOfDivisible_3}.");
-            Console.WriteLine($"Сумма чисел кратных 5 = {sumOfDivisible_5}.");
-            Console.WriteLine($"Общая сумма кратных чисел = {sumOfDiv3AndDiv5}.");
-*/
+            Console.WriteLine($"Общая сумма кратных чисел = {sumOfDiv3AndDiv5}.");*/
+
 
             // ДЗ. Конвертер валют
             /*int balanceOf_X = 1000;
