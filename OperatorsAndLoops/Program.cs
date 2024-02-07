a86de0b5286d37ab135475f4ff8b7b475291420a
karpov2713 <karpov2713@gmail.com>
2024-02-07 22:59:05 +0300

    Убрал ДЗ Конвертер валют в отдельный проект.

--- OperatorsAndLoops/Program.cs
+++ OperatorsAndLoops/Program.cs
@@ -287,46 +287,8 @@ namespace OperatorsAndLoops
             Console.WriteLine($"Случайное число = {number}. Общая сумма кратных чисел = {sumOfDivisibleNumbers}.");*/
 
             // ДЗ. Конвертер валют
-            /*int balanceOfCurrencyX = 1000;
-            int balanceOfCurrencyY = 1000;
-            int conversionRate_X_To_Y = 35;
-            int conversionRate_Y_To_X = 42;
-            string operation;
-            int amountForConversion;
-
-            while (true)
-            {
-                Console.WriteLine($"Текущий баланс: валюта X = {balanceOfCurrencyX}, валюта Y = {balanceOfCurrencyY}.");
-                Console.WriteLine("Введите код операции: 1 - обмен X на Y, 2 - обмен Y на X, exit - выход из программы.");
-                operation = Console.ReadLine();
-
-                if (operation == "1")
-                {
-                    Console.WriteLine("Какую сумму желаете обменять? ");
-                    amountForConversion = Convert.ToInt32(Console.ReadLine());
-
-                    balanceOfCurrencyX += amountForConversion * conversionRate_X_To_Y;
-                    balanceOfCurrencyY -= amountForConversion * conversionRate_X_To_Y;
-                }
-                else if (operation == "2")
-                {
-                    Console.WriteLine("Какую сумму желаете обменять? ");
-                    amountForConversion = Convert.ToInt32(Console.ReadLine());
-
-                    balanceOfCurrencyX += amountForConversion * conversionRate_Y_To_X;
-                    balanceOfCurrencyY -= amountForConversion * conversionRate_Y_To_X;
-                }
-                else if (operation == "exit")
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Код операции введен неверно, попробуйте ещё раз!");
-                }
-            }
-
-            Console.WriteLine($"Итоговый баланс: валюта X = {balanceOfCurrencyX}, валюта Y = {balanceOfCurrencyY}.");*/
+            // См. Проект CW_Module1_HW_CurrencyExchange
+            // Отличный результат по отзыву ментора.
 
             // ДЗ. Кратные числа
             /*int number;
