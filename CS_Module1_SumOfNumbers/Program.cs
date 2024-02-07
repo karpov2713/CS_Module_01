ba97cb8cedf8e087331a461973d55318b549425a
karpov2713 <karpov2713@gmail.com>
2024-02-07 10:47:43 +0300

    ДЗ Сумма чисел. Исправлены замечания. Попытка 5

--- CS_Module1_SumOfNumbers/Program.cs
+++ CS_Module1_SumOfNumbers/Program.cs
@@ -8,32 +8,21 @@ namespace CS_Module1_SumOfNumbers
         {
             Random random = new Random();
 
-            int number;
+            float number;
             int maxValue = 101;
-            int sumOfDivisible_3 = 0;
-            int sumOfDivisible_5 = 0;
-            int sumOfDiv3AndDiv5 = 0;
-            int figure_3 = 3;
-            int figure_5 = 5;
+            int sumOfDivisibleNumbers = 0;
 
             number = random.Next(0, maxValue);
 
             for (int i = 0; i <= number; i++)
             {
-                if (i % figure_3 == 0)
+                if (i % 3 == 0 || i % 5 == 0)
                 {
-                    sumOfDivisible_3 += i;
+                    sumOfDivisibleNumbers += i;
                 }
-                else if (i % figure_5 == 0)
-                {
-                    sumOfDivisible_5 += i;
-                }
-
-                sumOfDiv3AndDiv5 = sumOfDivisible_3 + sumOfDivisible_5;
             }
 
-            Console.WriteLine($"Случайное число = {number}.");
-            Console.WriteLine($"Общая сумма кратных чисел = {sumOfDiv3AndDiv5}.");
+            Console.WriteLine($"Случайное число = {number}. Общая сумма кратных чисел = {sumOfDivisibleNumbers}.");
         }
     }
 }
