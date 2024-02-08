2a81c2258db7c8cfd5b1975ec9ed26b48fd91f65
karpov2713 <karpov2713@gmail.com>
2024-02-08 20:12:40 +0300

    ДЗ Программа под паролем. 1 попытка сдать.

--- ProgramPassword/Program.cs
+++ ProgramPassword/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProgramPassword
 {
@@ -10,6 +6,26 @@ namespace ProgramPassword
     {
         static void Main(string[] args)
         {
+            string password = "qwer1234";
+            int attempts = 3;
+            string secretMessage = "Hello world!";
+
+            for (int i = attempts; i > 0; i--)
+            {
+                Console.Write($"У вас осталось {i} попытки.\t");
+                Console.Write("Введите пароль: ");
+
+                if (password == Console.ReadLine())
+                {
+                    Console.WriteLine($"Пароль принят! {secretMessage}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный пароль!");
+                }
+
+            }
 
         }
     }
